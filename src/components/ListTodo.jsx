b2b6c9c14b0c46fba72e6d385410728a5dd3835d
alfian <devsroot8@gmail.com>
2023-10-27 18:36:41 +0700

    feat: filter todo activity

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteTodo, editTodo, getTodo } from "../redux/reducers/todo-reducer";
+import {
+  deleteTodo,
+  editStatus,
+  editTodo,
+  getTodo,
+} from "../redux/reducers/todo-reducer";
 import { BiSolidTrashAlt, BiSolidPencil, BiSolidSave } from "react-icons/bi";
 import { MdCancel } from "react-icons/md";
+import { BsCheckCircleFill } from "react-icons/bs";
+import { FiCircle } from "react-icons/fi";
 
 export default function ListTodo() {
   const [isEdit, setIsEdit] = useState(null);
   const [editValue, setEditValue] = useState("");
+  const [filter, setFilter] = useState("all");
+  const [error, setError] = useState("");
 
   const { isLoading, todos } = useSelector((state) => state.todo);
   const dispatch = useDispatch();
@@ -25,20 +34,60 @@ export default function ListTodo() {
   };
 
   const editButtonHandler = (id) => {
-    dispatch(editTodo(id, editValue));
-    setIsEdit(null);
+    try {
+      if (editValue == "") {
+        throw new Error("value cannot empty");
+      }
+
+      dispatch(editTodo(id, editValue));
+      setIsEdit(null);
+      setError("");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const statusButtonHandler = (id, currentStatus) => {
+    const newStatus = !currentStatus;
+    dispatch(editStatus(id, newStatus));
+  };
+
+  const filteredTodos = todos.filter((item) => {
+    if (filter === "active") {
+      return item.status === true;
+    } else if (filter === "completed") {
+      return item.status === false;
+    }
+    return true;
+  });
+
+  const setActiveFilter = (filterName) => {
+    return filter === filterName
+      ? "bg-black text-white"
+      : "bg-zinc-100 hover:bg-black hover:text-white";
   };
 
   return (
     <>
       <div className="flex gap-2">
-        <button className="py-1 px-2 border rounded-md bg-black text-white duration-50">
+        <button
+          className={`py-1 px-2 border rounded-md ${setActiveFilter("all")}`}
+          onClick={() => setFilter("all")}
+        >
           all
         </button>
-        <button className="py-1 px-2 border rounded-md bg-zinc-100 hover:bg-black hover:text-white duration-50">
+        <button
+          className={`py-1 px-2 border rounded-md ${setActiveFilter("active")}`}
+          onClick={() => setFilter("active")}
+        >
           active
         </button>
-        <button className="py-1 px-2 border rounded-md bg-zinc-100 hover:bg-black hover:text-white duration-50">
+        <button
+          className={`py-1 px-2 border rounded-md ${setActiveFilter(
+            "completed"
+          )}`}
+          onClick={() => setFilter("completed")}
+        >
           completed
         </button>
       </div>
@@ -57,18 +106,25 @@ export default function ListTodo() {
             </h1>
           ) : (
             <ul>
-              {todos.map((item) => (
+              {filteredTodos.map((item) => (
                 <li
                   key={item.id}
                   className="w-full flex justify-between items-center my-8 gap-8"
                 >
                   {isEdit === item.id ? (
                     <>
-                      <input
-                        className="font-semibold bg-white border border-black p-2 w-full"
-                        value={editValue}
-                        onChange={(e) => setEditValue(e.target.value)}
-                      />
+                      <div className="flex flex-col justify-center">
+                        <input
+                          className="font-semibold bg-white border border-black p-2 w-full"
+                          value={editValue}
+                          onChange={(e) => setEditValue(e.target.value)}
+                        />
+                        {error && (
+                          <span className="text-red-500 font-semibold text-xs p-1">
+                            * {error}
+                          </span>
+                        )}
+                      </div>
                       <div className="flex gap-2">
                         <button
                           className="p-2 bg-zinc-800 text-white rounded-sm"
@@ -86,10 +142,17 @@ export default function ListTodo() {
                     </>
                   ) : (
                     <>
-                      <div className="flex">
+                      <div className="flex items-center">
+                        <button
+                          onClick={() =>
+                            statusButtonHandler(item.id, item.status)
+                          }
+                        >
+                          {item.status ? <FiCircle /> : <BsCheckCircleFill />}
+                        </button>
                         <span className="p-2">{item.todo}</span>
                       </div>
-                      <div className="flex gap-2">
+                      <div className={item.status ? "flex gap-2" : "hidden"}>
                         <button
                           className="p-2 bg-zinc-800 text-white rounded-sm"
                           onClick={() => editTodoHandler(item.id, item.todo)}
