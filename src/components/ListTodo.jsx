b946c366b5d0375e251c9f028205183c60cc3c1a
alfian <devsroot8@gmail.com>
2023-10-27 10:41:08 +0700

    fix: logic while activity render

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -8,8 +8,8 @@ export default function ListTodo() {
   const [isEdit, setIsEdit] = useState(null);
   const [editValue, setEditValue] = useState("");
 
+  const { isLoading, todos } = useSelector((state) => state.todo);
   const dispatch = useDispatch();
-  const { todos, isLoading } = useSelector((state) => state.todo);
 
   useEffect(() => {
     dispatch(getTodo());
@@ -42,11 +42,7 @@ export default function ListTodo() {
           completed
         </button>
       </div>
-      {todos.length === 0 ? (
-        <h1 className="flex justify-center mt-24 text-lg font-semibold">
-          there is no activity
-        </h1>
-      ) : isLoading ? (
+      {isLoading ? (
         <div className="flex justify-center mt-24">
           <div
             className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
@@ -54,56 +50,66 @@ export default function ListTodo() {
           ></div>
         </div>
       ) : (
-        <ul>
-          {todos.map((item) => (
-            <li
-              key={item.id}
-              className="w-full flex justify-between items-center my-8 gap-8"
-            >
-              {isEdit === item.id ? (
-                <>
-                  <input
-                    className="font-semibold bg-white border border-black p-2 w-full"
-                    value={editValue}
-                    onChange={(e) => setEditValue(e.target.value)}
-                  />
-                  <div className="flex gap-2">
-                    <button
-                      className="p-2 bg-zinc-800 text-white rounded-sm"
-                      onClick={() => editButtonHandler(item.id)}
-                    >
-                      <BiSolidSave className="text-white hover:text-green-500" />
-                    </button>
-                    <button
-                      className="p-2 bg-zinc-800 text-white rounded-sm"
-                      onClick={() => setIsEdit(null)}
-                    >
-                      <MdCancel />
-                    </button>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <span className="p-2">{item.todo}</span>
-                  <div className="flex gap-2">
-                    <button
-                      className="p-2 bg-zinc-800 text-white rounded-sm"
-                      onClick={() => editTodoHandler(item.id, item.todo)}
-                    >
-                      <BiSolidPencil className="text-white hover:text-yellow-400" />
-                    </button>
-                    <button
-                      className="p-2 bg-zinc-800 text-white rounded-sm"
-                      onClick={() => deleteButtonHandler(item.id)}
-                    >
-                      <BiSolidTrashAlt className="text-white hover:text-red-500" />
-                    </button>
-                  </div>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
+        <>
+          {todos.length === 0 ? (
+            <h1 className="flex justify-center mt-24 text-lg font-semibold">
+              there is no activity
+            </h1>
+          ) : (
+            <ul>
+              {todos.map((item) => (
+                <li
+                  key={item.id}
+                  className="w-full flex justify-between items-center my-8 gap-8"
+                >
+                  {isEdit === item.id ? (
+                    <>
+                      <input
+                        className="font-semibold bg-white border border-black p-2 w-full"
+                        value={editValue}
+                        onChange={(e) => setEditValue(e.target.value)}
+                      />
+                      <div className="flex gap-2">
+                        <button
+                          className="p-2 bg-zinc-800 text-white rounded-sm"
+                          onClick={() => editButtonHandler(item.id)}
+                        >
+                          <BiSolidSave className="text-white hover:text-green-500" />
+                        </button>
+                        <button
+                          className="p-2 bg-zinc-800 text-white rounded-sm"
+                          onClick={() => setIsEdit(null)}
+                        >
+                          <MdCancel />
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="flex">
+                        <span className="p-2">{item.todo}</span>
+                      </div>
+                      <div className="flex gap-2">
+                        <button
+                          className="p-2 bg-zinc-800 text-white rounded-sm"
+                          onClick={() => editTodoHandler(item.id, item.todo)}
+                        >
+                          <BiSolidPencil className="text-white hover:text-yellow-400" />
+                        </button>
+                        <button
+                          className="p-2 bg-zinc-800 text-white rounded-sm"
+                          onClick={() => deleteButtonHandler(item.id)}
+                        >
+                          <BiSolidTrashAlt className="text-white hover:text-red-500" />
+                        </button>
+                      </div>
+                    </>
+                  )}
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
       )}
     </>
   );
